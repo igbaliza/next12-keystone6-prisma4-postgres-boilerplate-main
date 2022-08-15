b18deae801cdf6f55b5e70d582e3ac9732d4b749
Igor Baliza <igorlink_93@hotmail.com>
2022-08-15 10:03:38 -0300

    add-halve-logic

--- keystone.ts
+++ keystone.ts
@@ -1,28 +1,30 @@
-import { config } from '@keystone-6/core';
-import { statelessSessions } from '@keystone-6/core/session';
-import { createAuth } from '@keystone-6/auth';
-import { PrizePool } from './src/schemas/prize-pool';
-import { Halve } from './src/schemas/halve';
-import { User } from './src/schemas/user';
+import { config } from "@keystone-6/core";
+import { statelessSessions } from "@keystone-6/core/session";
+import { createAuth } from "@keystone-6/auth";
+import { PrizePool } from "./src/schemas/prize-pool";
+import { Halve } from "./src/schemas/halve";
+import { User } from "./src/schemas/user";
+import { Context } from ".keystone/types";
+import { makeHalves } from "./src/rest/halves";
 
 const auth = createAuth({
-  identityField: 'name',
-  secretField: 'password',
-  listKey: 'User',
+  identityField: "name",
+  secretField: "password",
+  listKey: "User",
   sessionData: `id name`,
   initFirstItem: {
-    fields: ['name', 'password']
+    fields: ["name", "password"],
   },
 });
 
 export default auth.withAuth(
   config({
     db: {
-      provider: 'postgresql',
+      provider: "postgresql",
       url: String(process.env.DATABASE_URL),
       enableLogging: true,
       useMigrations: true,
-      idField: { kind: 'uuid' },
+      idField: { kind: "uuid" },
       shadowDatabaseUrl: String(process.env.SHADOW_DATABASE_URL),
     },
     experimental: {
@@ -30,8 +32,22 @@ export default auth.withAuth(
       generateNodeAPI: true,
     },
     lists: { PrizePool, Halve, User },
-    session: statelessSessions({ 
-      secret: String(process.env.SESSION_SECRET) || 'c4f190f9-2751-4eb0-9540-6f36ecf9a8d7'
-    })
+    session: statelessSessions({
+      secret: "c4f190f9-2751-4eb0-9540-6f36ecf9a8d7", //String(process.env.SESSION_SECRET),
+    }),
+    server: {
+      cors: {
+        origin: [String(process.env.PAGE_URL)],
+        credentials: true,
+      },
+      extendExpressApp: (app, createContext) => {
+        app.use("/api", async (request, response, next) => {
+          (request as any).context = await createContext(request, response);
+          next();
+        });
+
+        app.get("/api/makehalves", makeHalves);
+      },
+    },
   })
 );
--- src/schemas/halve.ts
+++ src/schemas/halve.ts
@@ -1,40 +1,68 @@
-import { integer, timestamp, relationship } from '@keystone-6/core/fields';
-import { list } from '@keystone-6/core';
+import {
+  integer,
+  float,
+  timestamp,
+  relationship,
+} from "@keystone-6/core/fields";
+import { list } from "@keystone-6/core";
 
 export const Halve = list({
-    fields: {
-        newTicketRangeBegin: integer({
-            db: {
-                map: 'new_ticket_range_begin'
-            }
-        }),
-        newTicketRangeEnd: integer({
-            db: {
-                map: 'new_ticket_range_end'
-            }
-        }),
-        halveDateTime: timestamp({
-            db: {
-                map: 'have_date_time'
-            }
-        }),
-        prizePool: relationship({ 
-            ref: 'PrizePool.halves',
-            db: {
-                foreignKey: { 
-                    map: 'prize_pool'
-                }
-            }
-        }),
-    },
-    ui: {
-        itemView: {
-            defaultFieldMode: 'read'
+  fields: {
+    newTicketRangeBegin: integer({
+      db: {
+        map: "new_ticket_range_begin",
+      },
+    }),
+    newTicketRangeEnd: integer({
+      db: {
+        map: "new_ticket_range_end",
+      },
+    }),
+    ticketValue: float({
+      db: {
+        map: "ticket_value",
+      },
+    }),
+    remainingTickets: integer({
+      db: {
+        map: "remaining_tickets",
+      },
+    }),
+    halveDateTime: timestamp({
+      db: {
+        map: "halve_date_time",
+      },
+    }),
+    prizePool: relationship({
+      ref: "PrizePool.halves",
+      db: {
+        foreignKey: {
+          map: "prize_pool",
         },
-        hideCreate: true,
-        isHidden: true
+      },
+    }),
+  },
+  ui: {
+    itemView: {
+      defaultFieldMode: "read",
     },
-    db: {
-        map: 'halves'
-    }
+    listView: {
+      initialSort: {
+        field: "halveDateTime",
+        direction: "DESC",
+      },
+      initialColumns: [
+        "halveDateTime",
+        "newTicketRangeBegin",
+        "newTicketRangeEnd",
+        "ticketValue",
+        "remainingTickets",
+      ],
+    },
+    hideCreate: true,
+    isHidden: true,
+  },
+  db: {
+    map: "halves",
+  },
 });
